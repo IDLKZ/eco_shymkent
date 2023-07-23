4ca9e95e4509771b8d3b2df8071239d48f0892db
misti <idlkz2019@gmail.com>
2023-07-23 18:00:36 +0600

    Statistics

--- resources/views/layouts/front/front-layout.blade.php
+++ resources/views/layouts/front/front-layout.blade.php
@@ -50,7 +50,7 @@
 <!-- Navbar Start -->
 <nav class="navbar navbar-expand-lg bg-white navbar-light sticky-top p-0">
     <a href="/" class="navbar-brand d-flex align-items-center px-4 px-lg-5">
-        <h1 class="m-0"><span class="text-success">ECO</span> <span class="text-danger">Shymkent</span></h1>
+        <h1 class="m-0"><span class="text-success">ZHASYL</span> <span class="text-danger">Shymkent</span></h1>
     </a>
     <button type="button" class="navbar-toggler me-4" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
         <span class="navbar-toggler-icon"></span>
