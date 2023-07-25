30ccbee3ed3ab5c72d2fd897f075e1cae7edc0fa
IDL <idlkz2019@gmail.com>
2023-07-25 14:20:33 +0600

    commit AddUserPlace.php add-user-place.blade.php

--- app/Http/Livewire/Admin/User/AddUserPlace.php
+++ app/Http/Livewire/Admin/User/AddUserPlace.php
@@ -2,34 +2,52 @@
 
 namespace App\Http\Livewire\Admin\User;
 
+use App\Models\Area;
 use App\Models\Place;
 use App\Models\User;
 use App\Models\UserPlace;
 use Livewire\Component;
+use Livewire\WithPagination;
 
 class AddUserPlace extends Component
 {
+    use WithPagination;
+    protected $paginationTheme = 'bootstrap';
     public User $user;
     public $search;
     public $places;
+    public $areas;
+    public $areaId = 0;
+    public $links;
+    public bool $show = false;
     public $userPlaces;
 
-    public function mount($user)
+    public function mount($user): void
     {
         $this->userPlaces = $user->user_places;
+        $this->areas = Area::all();
     }
-    public function updatedSearch()
+
+    public function getPlacesByAreaId(): void
     {
-        if ($this->search == '') {
-            $this->places = [];
+        if ($this->areaId != 0) {
+            $this->show = true;
         } else {
-            $this->places = Place::where('title_ru', 'like', '%'.$this->search.'%')->get();
+            $this->show = false;
         }
     }
 
+    public function updatedSearch(): void
+    {
+        $this->gotoPage(1);
+//        if ($this->search == '') {
+//            $this->places = [];
+//        } else {
+//            $this->places = Place::with('markers')->where('title_ru', 'like', '%'.$this->search.'%')->get();
+//        }
+    }
 
-
-    public function addPlace($place_id)
+    public function addPlace($place_id): void
     {
         UserPlace::updateOrCreate([
            'user_id' => $this->user->id,
@@ -43,17 +61,32 @@ class AddUserPlace extends Component
         $this->renderPlaces();
     }
 
-    public function renderPlaces()
+    public function renderPlaces(): void
     {
         $this->userPlaces = UserPlace::with('place')->where('user_id', $this->user->id)->get();
     }
     public function submit()
     {
-
         return redirect(route('user.index'));
     }
     public function render()
     {
-        return view('livewire.admin.user.add-user-place');
+        if ($this->areaId == 0) {
+            $placesWithMarkers = Place::withCount('markers')->orderBy('markers_count', 'asc')->paginate(15);
+        } else {
+            if ($this->search == '') {
+                $placesWithMarkers = Place::withCount('markers')
+                    ->orderBy('markers_count', 'asc')
+                    ->where('area_id', $this->areaId)
+                    ->paginate(15);
+            } else {
+                $placesWithMarkers = Place::withCount('markers')
+                    ->orderBy('markers_count', 'asc')
+                    ->where('area_id', $this->areaId)
+                    ->where('title_ru', 'like', '%'.$this->search.'%')
+                    ->paginate(15)->appends('search', $this->search);
+            }
+        }
+        return view('livewire.admin.user.add-user-place', ['placesWithMarkers' => $placesWithMarkers]);
     }
 }
